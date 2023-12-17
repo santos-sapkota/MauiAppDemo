e8a4f0ae2021c4d23d7bd3755cb012ab9bda000b
SANTOSH <SANTOSH@ASUS>
2023-12-17 06:29:12 +0545

    Add project files.

---
+++ MauiAppDemo/Models/ToDoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppDemo.Models
+{
+    internal class ToDoModel
+    {
+        public int ID { get; set; }
+        public string? Task { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
