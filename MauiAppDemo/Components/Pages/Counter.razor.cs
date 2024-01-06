33ec55b18542206854f582b6a7706965f13d4349
Santosh Sapkota <santos.sapk1997@gmail.com>
2024-01-06 20:57:56 +0545

    Inject on CS

--- MauiAppDemo/Components/Pages/Counter.razor.cs
+++ MauiAppDemo/Components/Pages/Counter.razor.cs
@@ -1,12 +1,18 @@
+using Microsoft.AspNetCore.Components;
+
 namespace MauiAppDemo.Components.Pages
 {
     public partial class Counter
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         private int currentCount = 0;
 
         private void IncrementCount()
         {
             currentCount++;
+            NavigationManager.NavigateTo("/weather");
         }
     }
 }
