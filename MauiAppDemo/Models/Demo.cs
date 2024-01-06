a02d541a90e8ace5282286f4dd57bd062d87f935
Santosh Sapkota <santos.sapk1997@gmail.com>
2024-01-06 21:13:17 +0545

    Parameter Examples

---
+++ MauiAppDemo/Models/Demo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppDemo.Models
+{
+    public class Demo
+    {
+        public int Value { get; set; } = 10;
+    }
+}
